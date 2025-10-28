734e4da6088fb8ba3a55b909bc12b6f8a47480a3
Simón Aguirre <siaguirre@uade.edu.ar>
2025-10-28 18:50:39 -0300

    agrego mamushka y multipilaTDA

---
+++ src/main/java/ar/edu/uade/progra2/grupo2/parte1/ejercicio3/MultipilaTDA.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.progra2.grupo2.parte1.ejercicio3;
+
+import tda.PilaTDA;
+
+public interface MultipilaTDA {
+    public void apilar (PilaTDA valores); 
+
+    public void desapilar (PilaTDA valores); 
+
+    public PilaTDA tope (int cantidad); 
+    public void inicializarPila(); 
+    public boolean pilaVacia();
+}
